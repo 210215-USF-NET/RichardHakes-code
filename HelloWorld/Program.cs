8d4cdecf34ba80d9cd0ec354594330e63cebcae0
Rich Hakes <rjhakes@yahoo.com>
2021-02-15 14:40:51 -0700

    Added initial program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;   //import package
+
+namespace HelloWorld    //package Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
